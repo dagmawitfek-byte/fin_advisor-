8be61e0a0c8d9fc1cdc78b9cfec74c87145fee4e
dagmawitfek-byte <dagmawitfek@gmail.com>
2026-09-16 09:46:11 +0300

    Update main.dart

--- fin_advisor-main/lib/main.dart
+++ fin_advisor-main/lib/main.dart
@@ -24,11 +24,9 @@ class _FinAdvisorAppState extends State<FinAdvisorApp> {
   void initState() {
     super.initState();
     // Initialize SMS listener after app is fully loaded
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
+        WidgetsBinding.instance.addPostFrameCallback((_) async {
       await SmsListener().initialize(context);
-      if (mounted) SmsListener().startListeningForNewSms(context);
     });
-  }
 
   @override
   Widget build(BuildContext context) {
